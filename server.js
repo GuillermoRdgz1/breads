93884a804dea0a90647d1807a25c0231babd72c1
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-01-23 18:55:32 -0800

    404 etc

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ console.log(PORT)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
 //ROUTES//
@@ -26,7 +27,14 @@ const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 
+//404//
+app.get('*', (req, res) => {
+    res.send('404')
+})
+
+
 //LISTEN//
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
+
